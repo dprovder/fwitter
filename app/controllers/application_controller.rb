defde50cba2f96e5d1ff08b430f359b94f6a2ffb
dprovder <danielprovder@gmail.com>
2015-07-08 16:38:30 +0000

    database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,23 @@
 require_relative "../../config/environment"
+require_relative "../models/fweet"
 class ApplicationController < Sinatra::Base
 
+  configure do
+    set :public_folder, "public"
+    set :views, "app/views"
+  end
+  
   get "/" do
-    "hello World"
+#     @fweets = Fweet.all
+    erb :index
+  end
+  
+  
+  post "/" do
+#     @fweet = Fweet.new(params[:username],params[:fweet]).save
+#     @fweets = Fweet.all
+    fweet.create(:user => params[:user], :content => params [:fweet])
+    erb :index
   end
   
 end
---
+++ app/models/fweet.rb
@@ -0,0 +1,22 @@
+require_relative "../../config/environment"
+
+class Fweet < ActiveRecord::Base
+  
+#   ALL_FWEETS = []
+#   attr_accessor :content
+#   attr_reader :user
+  
+#   def initialize(user, content)
+#     @user = user
+#     @content = content
+#     @retweets = 0
+#     ALL_FWEETS.push(self)
+#   end
+  
+#   def self.all
+#     ALL_FWEETS
+#   end
+  
+  
+  
+end
